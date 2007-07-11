df600b9b87b1435dee6b876a67fc8dcfff6322f8
joshcarter2006 <joshcarter2006@4da60275-9c2f-0410-b69a-795f42cc130f>
2007-07-11 18:36:02 +0000

    Adding Movable Type plugin version 1.0

---
+++ trunk/recaptcha-plugins/movabletype/Recaptcha.pl
@@ -0,0 +1,88 @@
+package MT::Plugin::Recaptcha;
+
+use MT;
+use MT::Plugin;
+use MT::JunkFilter qw(ABSTAIN);
+use HTTP::Request::Common qw(POST);
+use LWP::UserAgent;
+
+use vars qw($VERSION @ISA);
+@ISA = qw(MT::Plugin);
+$VERSION = 1.0;
+
+my $plugin = new MT::Plugin::Recaptcha({
+    name => 'Recaptcha',
+    version => $VERSION,
+    description => 'Uses Recaptcha system to reduce comment spam.',
+    author_name => 'Josh Carter',
+    author_link => 'http://multipart-mixed.com/',
+    plugin_link => 'http://multipart-mixed.com/software/recaptcha.html',
+    doc_link => 'http://multipart-mixed.com/software/recaptcha.html',
+    settings => new MT::PluginSettings([
+        ['public_key', { Default => 'YOUR_PUBLIC_KEY_HERE' }],
+        ['private_key', { Default => 'YOUR_PRIVATE_KEY_HERE' }]
+    ])
+});
+MT->add_plugin($plugin);
+
+sub instance {
+    $plugin
+}
+
+MT::Template::Context->add_tag('RecaptchaBox',
+    sub {
+        my $config = MT::Plugin::Recaptcha->instance->get_config_hash();
+        
+        return <<HTML
+<script type="text/javascript">
+var RecaptchaOptions = { theme : 'white' };
+</script>
+<script type="text/javascript"
+   src="http://api.recaptcha.net/challenge?k=$config->{'public_key'}">
+</script>
+
+<noscript>
+   <iframe src="http://api.recaptcha.net/noscript?k=$config->{'public_key'}"
+       height="300" width="500" frameborder="0"></iframe><br>
+   <textarea name="recaptcha_challenge_field" rows="3" cols="40">
+   </textarea>
+   <input type="hidden" name="recaptcha_response_field" 
+       value="manual_challenge">
+</noscript>        
+HTML
+    }
+);
+
+MT->add_callback('CommentErrorFilter', 1, $plugin, sub {
+    my ($eh, $app, $query) = @_;
+
+    my $config = MT::Plugin::Recaptcha->instance->get_config_hash();
+
+    my $ua = LWP::UserAgent->new();
+    my $req = POST('http://api-verify.recaptcha.net/verify',
+        ['privatekey' => $config->{'private_key'},
+         'remoteip' => $query->remote_addr(),
+         'challenge' => $query->param('recaptcha_challenge_field'),
+         'response' => $query->param('recaptcha_response_field')]);
+    my $response = $ua->request($req);
+    
+    if ($response->is_success()) {
+        my @result = split(/\n/, $response->content());
+        
+        if ($result[0] eq 'true') {
+            return undef;
+        }
+        else {
+            return "Failed CAPTCHA verification; please use your browser's " .
+                   "back button and re-enter the verification words. " .
+                   "Error code: " . $result[1]
+        }
+    }
+    else {
+        return "Failed connecting to CAPCHTA verification service; please " .
+               "try again later and/or contact me via email about this " .
+               "error message: " . $response->status_line();
+    }
+});
+
+1;
