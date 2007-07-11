6362e6821747f2af8b3f4190f736c647eb5b8f22
joshcarter2006 <joshcarter2006@4da60275-9c2f-0410-b69a-795f42cc130f>
2007-07-11 18:36:44 +0000

    Adding Movable Type plugin version 1.1

--- trunk/recaptcha-plugins/movabletype/Recaptcha.pl
+++ trunk/recaptcha-plugins/movabletype/Recaptcha.pl
@@ -8,7 +8,7 @@ use LWP::UserAgent;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(MT::Plugin);
-$VERSION = 1.0;
+$VERSION = 1.1;
 
 my $plugin = new MT::Plugin::Recaptcha({
     name => 'Recaptcha',
@@ -42,12 +42,9 @@ var RecaptchaOptions = { theme : 'white' };
 </script>
 
 <noscript>
-   <iframe src="http://api.recaptcha.net/noscript?k=$config->{'public_key'}"
-       height="300" width="500" frameborder="0"></iframe><br>
-   <textarea name="recaptcha_challenge_field" rows="3" cols="40">
-   </textarea>
-   <input type="hidden" name="recaptcha_response_field" 
-       value="manual_challenge">
+   <iframe src="http://api.recaptcha.net/noscript?k=$config->{'public_key'}" height="300" width="500" frameborder="0"></iframe><br>
+   <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+   <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
 </noscript>        
 HTML
     }
@@ -57,13 +54,27 @@ MT->add_callback('CommentErrorFilter', 1, $plugin, sub {
     my ($eh, $app, $query) = @_;
 
     my $config = MT::Plugin::Recaptcha->instance->get_config_hash();
+    my $challenge_field = $query->param('recaptcha_challenge_field');
+    my $response_field = $query->param('recaptcha_response_field');
+    
+    unless ($challenge_field && length($challenge_field) > 0) {
+        return "Failed CAPTCHA verification; please use your browser's " .
+               "back button and re-enter the verification words. " .
+               "Error: challenge field cannot be empty.";
+    }
+
+    unless ($response_field && length($response_field) > 0) {
+        return "Failed CAPTCHA verification; please use your browser's " .
+               "back button and re-enter the verification words. " .
+               "Error: response field cannot be empty.";
+    }
 
     my $ua = LWP::UserAgent->new();
     my $req = POST('http://api-verify.recaptcha.net/verify',
         ['privatekey' => $config->{'private_key'},
          'remoteip' => $query->remote_addr(),
-         'challenge' => $query->param('recaptcha_challenge_field'),
-         'response' => $query->param('recaptcha_response_field')]);
+         'challenge' => $challenge_field,
+         'response' => $response_field]);
     my $response = $ua->request($req);
     
     if ($response->is_success()) {
