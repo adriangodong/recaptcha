f81085bb55df631baabaf3fc7c39906775023f79
joshcarter2006 <joshcarter2006@4da60275-9c2f-0410-b69a-795f42cc130f>
2007-07-11 18:42:53 +0000

    Movable Type plugin version 1.2. Changes:
    - Public/private key moved from source to MT plugins GUI, where it
      should be.
    - Multiple blogs on the same MT install can use individual
      public/private key pairs.
    - Plugin can be enabled/disabled from GUI.
    - Changed user-visible instances of "Recaptcha" to "reCAPTCHA".
    - Added BSD license text.

--- trunk/recaptcha-plugins/movabletype/Recaptcha.pl
+++ trunk/recaptcha-plugins/movabletype/Recaptcha.pl
@@ -1,3 +1,42 @@
+# This is a Movable Type plugin that handles reCAPTCHA authorization of 
+# comment posts.
+#
+# Documentation and latest version:
+#   http://multipart-mixed.com/software/recaptcha.html
+# More info on reCAPTCHA:
+#   http://recaptcha.net/
+# Get a reCAPTCHA API Key:
+#   http://recaptcha.net/api/getkey
+# Discussion group:
+#   http://groups.google.com/group/recaptcha
+# 
+# Copyright (c) 2007 Josh Carter
+#
+# AUTHORS:
+#   Josh Carter
+# CONTRIBUTIONS FROM:
+#   Ian Peters
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 package MT::Plugin::Recaptcha;
 
 use MT;
@@ -8,19 +47,21 @@ use LWP::UserAgent;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(MT::Plugin);
-$VERSION = 1.1;
+$VERSION = 1.2;
 
 my $plugin = new MT::Plugin::Recaptcha({
-    name => 'Recaptcha',
+    name => 'reCAPTCHA',
     version => $VERSION,
-    description => 'Uses Recaptcha system to reduce comment spam.',
+    description => 'Uses reCAPTCHA system to reduce comment spam.',
     author_name => 'Josh Carter',
     author_link => 'http://multipart-mixed.com/',
     plugin_link => 'http://multipart-mixed.com/software/recaptcha.html',
     doc_link => 'http://multipart-mixed.com/software/recaptcha.html',
+    blog_config_template => \&config_tmpl,
     settings => new MT::PluginSettings([
-        ['public_key', { Default => 'YOUR_PUBLIC_KEY_HERE' }],
-        ['private_key', { Default => 'YOUR_PRIVATE_KEY_HERE' }]
+        ['public_key', { Default => '' }],
+        ['private_key', { Default => '' }],
+        ['enabled', { Default => 0 }]
     ])
 });
 MT->add_plugin($plugin);
@@ -29,16 +70,20 @@ sub instance {
     $plugin
 }
 
-MT::Template::Context->add_tag('RecaptchaBox',
-    sub {
-        my $config = MT::Plugin::Recaptcha->instance->get_config_hash();
-        
-        return <<HTML
+MT::Template::Context->add_tag('RecaptchaBox', sub {
+    my $ctx = shift;
+    my $blog_id = $ctx->stash('blog_id');
+    my $config = MT::Plugin::Recaptcha->instance->get_config_hash("blog:$blog_id");
+
+    unless ($config->{'enabled'}) {
+        return '';
+    }
+
+    return <<HTML
 <script type="text/javascript">
 var RecaptchaOptions = { theme : 'white' };
 </script>
-<script type="text/javascript"
-   src="http://api.recaptcha.net/challenge?k=$config->{'public_key'}">
+<script type="text/javascript" src="http://api.recaptcha.net/challenge?k=$config->{'public_key'}">
 </script>
 
 <noscript>
@@ -51,9 +96,15 @@ HTML
 );
 
 MT->add_callback('CommentErrorFilter', 1, $plugin, sub {
-    my ($eh, $app, $query) = @_;
+    my ($eh, $app, $entry) = @_;
+    my $query = $app->{query};
+    my $blog_id = $entry->blog_id;
+    my $config = MT::Plugin::Recaptcha->instance->get_config_hash("blog:$blog_id");
 
-    my $config = MT::Plugin::Recaptcha->instance->get_config_hash();
+    unless ($config->{'enabled'}) {
+        return undef;
+    }
+
     my $challenge_field = $query->param('recaptcha_challenge_field');
     my $response_field = $query->param('recaptcha_response_field');
     
@@ -72,7 +123,7 @@ MT->add_callback('CommentErrorFilter', 1, $plugin, sub {
     my $ua = LWP::UserAgent->new();
     my $req = POST('http://api-verify.recaptcha.net/verify',
         ['privatekey' => $config->{'private_key'},
-         'remoteip' => $query->remote_addr(),
+         'remoteip' => $app->remote_ip,
          'challenge' => $challenge_field,
          'response' => $response_field]);
     my $response = $ua->request($req);
@@ -95,5 +146,26 @@ MT->add_callback('CommentErrorFilter', 1, $plugin, sub {
                "error message: " . $response->status_line();
     }
 });
+
+sub config_tmpl {
+    my $tmpl = <<'EOT';
+<p>reCAPTCHA is a CAPTCHA implementation that protects your blog from spam, while helping digitize old texts.  For more information about reCAPTCHA, and to sign up for your own keys, please visit <a href="http://recaptcha.net/">recaptcha.net</a>.</p>
+<div class="setting">
+    <label for="enabled"><input type="checkbox" name="enabled" id="enabled" value="1"<TMPL_IF NAME=ENABLED_1> checked="checked" </TMPL_IF>/> Enable reCAPTCHA filtering for this blog</label>
+</div>
+<div class="setting">
+    <div class="label"><label for="public_key"><MT_TRANS phrase="Public Key:"></label></div>
+    <div class="field">
+    <input name="public_key" id="public_key" size="40" value="<TMPL_VAR NAME=PUBLIC_KEY ESCAPE=HTML>" />
+    </div>
+</div>
+<div class="setting">
+    <div class="label"><label for="private_key"><MT_TRANS phrase="Private Key:"></label></div>
+    <div class="field">
+    <input name="private_key" id="private_key" size="40" value="<TMPL_VAR NAME=PRIVATE_KEY ESCAPE=HTML>" />
+    </div>
+</div>
+EOT
+}
 
 1;
