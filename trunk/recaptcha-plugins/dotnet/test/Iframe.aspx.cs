4b1e338a2973408b04bd4f59d2de4e67c2efcb34
adrian.godong@gmail.com <adrian.godong@gmail.com@4da60275-9c2f-0410-b69a-795f42cc130f>
2011-07-27 23:37:59 +0000

    .NET Library: added iframe test page.

---
+++ trunk/recaptcha-plugins/dotnet/test/Iframe.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Recaptcha.Test
+{
+    public partial class Iframe : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
