7af861db340a6fcbfd5b7d4314047840069af584
adrian.godong@gmail.com <adrian.godong@gmail.com@4da60275-9c2f-0410-b69a-795f42cc130f>
2011-08-22 20:46:53 +0000

    .NET Library: added custom translation test page.

---
+++ trunk/recaptcha-plugins/dotnet/test/CustomTranslation.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecaptchaTest
+{
+    public partial class CustomTranslation : System.Web.UI.Page
+    {
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+
+            var customTranslation = new Dictionary<string, string>();
+            customTranslation.Add("instructions_visual", "Scrivi le due parole:");
+            customTranslation.Add("instructions_audio", "Trascrivi ci\u00f2 che senti:");
+            customTranslation.Add("play_again", "Riascolta la traccia audio");
+            customTranslation.Add("cant_hear_this", "Scarica la traccia in formato MP3");
+            customTranslation.Add("visual_challenge", "Modalit\u00e0 visiva");
+            customTranslation.Add("audio_challenge", "Modalit\u00e0 auditiva");
+            customTranslation.Add("refresh_btn", "Chiedi due nuove parole");
+            customTranslation.Add("help_btn", "Aiuto");
+            customTranslation.Add("incorrect_try_again", "Scorretto. Riprova.");
+
+            RecaptchaControl.CustomTranslations = customTranslation;
+
+        }
+
+
+        protected void RecaptchaButton_Click(object sender, EventArgs e)
+        {
+            if (this.Page.IsValid)
+            {
+                this.RecaptchaResult.Text = "Success";
+            }
+            else
+            {
+                this.RecaptchaResult.Text = this.RecaptchaControl.ErrorMessage;
+            }
+        }
+    }
+}
